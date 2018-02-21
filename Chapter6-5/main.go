7866cdfeac6b90ccc320286dcf4593998e2a7302
NUT\patthara <patthara.th@tnis.com>
2018-02-21 12:15:41 +0700

    Refactor code chapter 6-5

--- Chapter6-5/main.go
+++ Chapter6-5/main.go
@@ -6,20 +6,33 @@ import (
 
 func main() {
 	for number := 1; number <= 100; number++ {
-		fmt.Println(fizzbuzz(number))
+		fmt.Println(number, fizzbuzz(number))
 	}
 }
 
-func fizzbuzz(number int) (int, string) {
+func fizzbuzz(number int) string {
 
-	if number%15 == 0 {
-		return number, "FizzBuzz"
-	} else if number%3 == 0 {
-		return number, "Fizz"
-	} else if number%5 == 0 {
-		return number, "Buzz"
-	} else {
-		return number, ""
+	ln := [3]int{15, 3, 5}
+	str := [3]string{"FizzBuzz", "Fizz", "Buzz"}
+
+	for i := 0; i < 3; i++ {
+		if number%ln[i] == 0 {
+			return str[i]
+		}
+
 	}
+	// if number%ln[0] == 0 {
+	// 	return str[0]
+	// }
+
+	// if number%ln[1] == 0 {
+	// 	return str[1]
+	// }
+
+	// if number%ln[2] == 0 {
+	// 	return str[2]
+	// }
+
+	return ""
 
 }

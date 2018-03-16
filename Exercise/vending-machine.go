b7dc4b3bc38894ddd87995aff55ef4ef386b4ff8
NUT\patthara <patthara.th@tnis.com>
2018-03-16 11:46:49 +0700

    Modify vending-machine.go

--- Exercise/vending-machine.go
+++ Exercise/vending-machine.go
@@ -2,94 +2,8 @@ package main
 
 import (
 	"fmt"
-	"sort"
+	"github.com/Patthara-th/vendingMachine"
 )
-
-type vendingMachine struct {
-	current     int
-	coin        map[string]int
-	items       map[string]int
-	changecoins map[int]string
-	value       []int
-	insertedcoin		[]string
-}
-
-func newVendingMachine(x, y map[string]int) *vendingMachine {
-
-	value := make([]int, 0)
-	var changecoins map[int]string
-	changecoins = make(map[int]string)
-	for k, v := range coins {
-		changecoins[v] = k
-		value = append(value, v)
-	}
-	sort.Sort(sort.Reverse(sort.IntSlice(value)))
-	return &vendingMachine{0, x, y, changecoins, value,make([]string,0)}
-
-}
-
-func (v *vendingMachine) InsertCoin(c string) {
-
-	v.current += v.coin[c]
-	v.insertedcoin = append(v.insertedcoin,c)
-
-}
-
-func (v *vendingMachine) GetInsertedMoney() int {
-
-	return v.current
-
-}
-
-func (v *vendingMachine) SelectSD() string {
-
-	if v.current >= v.items["SD"] {
-		v.insertedcoin = make([]string,0)
-		v.current -= v.items["SD"]
-		if v.current > 0 {
-			return "SD" + v.getchange()
-		}
-		return "SD"
-	}	
-	return "Not Enough Coins"
-}
-
-func (v *vendingMachine) SelectCC() string {
-
-	if v.current >= v.items["CC"] {
-		v.insertedcoin = make([]string,0)
-		v.current -= v.items["CC"]
-		if v.current > 0 {
-			return "CC" + v.getchange()
-		}
-		return "CC"
-	}	
-	return "Not Enough Coins"
-
-}
-
-func (v *vendingMachine) getchange() string {
-		var change string
-	for _, x := range v.value {
-		for v.current >= x {
-			change += ", " + v.changecoins[x]
-			v.current -= x
-		}
-	}
-	return change
-}
-
-func (v *vendingMachine) CoinReturn() string {
-
-	var change string
-	
-	for _, x := range v.insertedcoin {
-			change += ", " + x			
-	}
-	v.insertedcoin = make([]string,0)
-	return change[2:len(change)]
-
-}
 
 var coins map[string]int
 var items map[string]int
@@ -111,7 +25,7 @@ func init() {
 
 func main() {
 
-	vm := newVendingMachine(coins, items)
+	vm := vendingMachine.NewVendingMachine(coins, items)
 
 	// Buy SD(soft drink) with exact change
 	vm.InsertCoin("T")
@@ -132,7 +46,7 @@ func main() {
 	// Start adding change but hit coin return
 	vm.InsertCoin("T")
 	vm.InsertCoin("TW")
-	vm.InsertCoin("F")	
+	vm.InsertCoin("F")
 	vm.InsertCoin("O")
 	vm.InsertCoin("T")
 	vm.InsertCoin("O")
